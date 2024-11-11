f1e38a29f60851ff6ac21daade354d9ce7228f14
Boutzi <joe@arkaans.com>
2024-11-11 19:15:29 +0100

    fix manipulatios

---
+++ exercices/2-intermédiaire/manipulations/arrays.js
@@ -0,0 +1,15 @@
+let fruits = ["apple", "banana", "orange"];
+let citrus = ["orange", "lemon"];
+
+// 1. Add 'grape' to the end of the 'fruits' array.
+
+// 2. Remove the first element from the 'fruits' array.
+
+// 3. Add 'kiwi' to the beginning of the 'fruits' array.
+
+// 4. Remove the last element from the 'fruits' array.
+
+// 5. Concatenate the 'fruits' and 'citrus' arrays into a new array named 'allFruits'.
+
+// 6. Log the 'allFruits' array to the console.
+// Expected output: ['kiwi', 'banana', 'orange', 'lemon']
---
+++ exercices/2-intermédiaire/manipulations/objets.js
@@ -0,0 +1,74 @@
+const data = [
+  {
+    id: 0,
+    name: "France",
+    continent: "Europe",
+    code: "FR",
+  },
+  {
+    id: 1,
+    name: "Etats-Unis",
+    continent: "Amérique",
+    code: "US",
+  },
+  {
+    id: 2,
+    name: "Royaume-Uni",
+    continent: "Europe",
+    code: "UK",
+  },
+  {
+    id: 3,
+    name: "Italie",
+    continent: "Europe",
+    code: "IT",
+  },
+  {
+    id: 4,
+    name: "Corée",
+    continent: "Asie",
+    code: "KR",
+  },
+  {
+    id: 5,
+    name: "Japon",
+    continent: "Asie",
+    code: "JP",
+  },
+  {
+    id: 6,
+    name: "Allemagne",
+    continent: "Europe",
+    code: "DE",
+  },
+  {
+    id: 7,
+    name: "Mexique",
+    continent: "Amérique",
+    code: "MX",
+  },
+  {
+    id: 8,
+    name: "Australie",
+    continent: "Océanie",
+    code: "AU",
+  },
+  {
+    id: 9,
+    name: "Chine",
+    continent: "Océanie",
+    code: "ZH",
+  },
+  {
+    id: 10,
+    name: "Espagne",
+    continent: "Europe",
+    code: "ES",
+  },
+];
+
+// Exercise 1: Find all countries in Asia and log their codes
+
+// Exercise 2: Create a new array with only the names of the countries
+
+// Exercise 3: Find the country with the code "US" and log its name

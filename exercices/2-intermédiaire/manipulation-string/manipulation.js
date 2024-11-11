83b3b8b4750304850584049298f754a552564c3c
Boutzi <joe@arkaans.com>
2024-11-11 18:38:33 +0100

    new manipulation string

---
+++ exercices/2-intermédiaire/manipulation-string/manipulation.js
@@ -0,0 +1,4 @@
+// Modifiez le prénom, l'âge, la ville et changez le retour à la ligne par un espace en manipulant la variable avec des méthodes javascript
+const text = "Bonjour, je m'appelle Joseph, j'ai 31 ans.\nJ'habite à Bordeaux.";
+
+console.log(text);

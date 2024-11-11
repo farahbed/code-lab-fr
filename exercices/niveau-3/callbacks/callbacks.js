3dfed8e23e1daa084458570131c08dd31dbf2e89
Boutzi <joe@arkaans.com>
2024-11-11 22:24:28 +0100

    new UI

---
+++ exercices/niveau-3/callbacks/callbacks.js
@@ -0,0 +1,6 @@
+const resultat = "Bravo";
+// 1. Créer une fonction "fetchData" qui appelle un callback et traite la variable "resultat"
+
+// 2. Créer une fonction callback "processData" qui traite les données en les affichant dans la console
+
+// Appel de la fonction fetchData avec processData comme callback
